eea1c63bd9bf6ceb82a708631fb82bf19978716e
Nazila Hasanova <159264619+NazilaHasanova2004@users.noreply.github.com>
2024-02-18 02:01:32 +0400

    sixth

---
+++ Lesson6/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = int.Parse(Console.ReadLine());
+
+            //int b = int.Parse(Console.ReadLine());
+            //PrintResult(a, b);
+
+            // Fibonacchi(a);
+
+            //Console.WriteLine(SumOfDigits(a));
+            string s = Console.ReadLine();
+            PrintString(s);
+            Console.ReadLine();
+
+        }
+        static void PrintResult(int a,int b)
+        {
+            Console.WriteLine($"The 1st number is:{a}");
+            Console.WriteLine($"The 2nd number is:{b}");
+
+        }
+        static void Fibonacchi(int n)
+        {
+            int a = 0, b = 1,c;
+            Console.Write(a+" "+b+" ");
+            for (int i = 0; i < n; i++)
+            {
+                c = a + b;
+                Console.Write(a+b+" ");
+                a = b;
+                b = c;
+               
+                
+            }
+        }
+        static int SumOfDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+           return sum;
+            
+
+        }
+        static void PrintString(string s)
+        {
+            int space = 0;
+            s = s.Trim();
+            s.ToCharArray();
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i]==' ')
+                {
+                    space++;
+
+                }
+            }
+            Console.WriteLine($"\"{s}\" contains {space} spaces");
+
+        }
+        static void Task123(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random r = new Random();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(0,100);
+            }
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+            int thirdMax = arr[2];
+            Console.WriteLine($"Third max number is :{thirdMax}");
+            Console.WriteLine("Four max number are :");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
