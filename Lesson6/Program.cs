2ce3ee88108bcd6b3dc2d319157a5384004ac0dd
NazilaHasanova2004 <159264619+NazilaHasanova2004@users.noreply.github.com>
2024-02-18 16:51:15 +0400

    Update Program.cs

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -97,8 +97,20 @@ namespace Les6
                 }
 
             } //ascending order
-            Array.Reverse(arr);
+             for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length-i-1; j++)
+                {
+                    if (arr[j + 1] > arr[j])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
 
+                }
+
+            } // descending order
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.Write(arr[i] + " ");
