41c631955384c7d09daac188e9bc8bcbc3e6dad2
Nazila Hasanova <159264619+NazilaHasanova2004@users.noreply.github.com>
2024-03-21 08:24:01 +0400

    16th

---
+++ Lesson16/Lesson2_a/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_a
+{
+    internal class Program
+    {
+        class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DateTime Date { get; set; }
+        }
+        static string url = @"C:\Users\Azay\Desktop";
+        static string pathfolder = @"C:\Users\Azay\Desktop\MyFolderrrr";
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(url, "MyFolderrrr");
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            dir.Create();
+            if (dir.Exists)
+            {
+                Console.WriteLine("Folder created");
+            }
+            string s;
+            do
+            {
+                Student st = new Student();
+                Console.WriteLine("Enter your ID:");
+                st.Id=int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter your Name:");
+                st.Name=Console.ReadLine();
+
+                Console.WriteLine("Enter your DateBirth:");
+                st.Date=DateTime.Parse(Console.ReadLine());
+
+                var filepath = Path.Combine(pathfolder, st.Name + ".txt");
+                string content = $"Your id is {st.Id},name is {st.Name} and DateBirth is {st.Date.Date}";
+                File.WriteAllText(filepath,content);
+                Console.WriteLine("If you want to add next person info,write Yes/or Not");
+                s = Console.ReadLine();
+            } while (s.ToLower()=="yes");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson16/Lesson2_b/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_b
+{
+    internal class Program
+    {
+        static string pathfolder = @"C:\Users\Azay\Desktop\MyFolderrrr";
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(pathfolder);
+            watcher.EnableRaisingEvents = true;
+
+            // Subscribe to the Created event
+            watcher.Created += OnCreated;
+
+            Console.WriteLine("Waiting for new student info files. Press 'q' to quit.");
+            while (true)
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Q)
+                    break;
+            }
+        }
+
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Created && !e.Name.StartsWith("."))
+            {
+                // read and visualize the contents of the new student info file
+                string filepath = Path.Combine(pathfolder, e.Name);
+                string studentinfo = File.ReadAllText(filepath);
+                Console.WriteLine($"new student info: {studentinfo}");
+
+                // wait for 1 second before processing the next file system change event
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
